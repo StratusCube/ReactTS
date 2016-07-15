921460ff5e2f5f5615cc2f834f66189c2b545db5
Mark Provenzano <Mark Provenzano>
2016-07-15 10:13:49 -0400

    modified _references

--- src/ReactTS/wwwroot/_references.js
+++ src/ReactTS/wwwroot/_references.js
@@ -1,12 +1,14 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../app/build/Foo.js" />
 /// <reference path="../app/build/HelloWorld.js" />
+/// <reference path="../app/helloworld.js" />
 /// <reference path="../app/index.js" />
 /// <reference path="../webpack.config.js" />
+/// <reference path="js/client - copy.bundle.js" />
 /// <reference path="js/client.bundle.js" />
 /// <reference path="js/client.js" />
+/// <reference path="js/server - copy.js" />
 /// <reference path="js/server.bundle.js" />
-/// <reference path="js/server.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
