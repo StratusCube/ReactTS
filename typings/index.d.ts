4f039c315b53966660025a9d0f100210b1bc49b0
mark.pro <mark.pro@stratuscube.com>
2016-07-17 11:37:35 -0400

    <<added>> jquery types to the project

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/materialize-css/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
 /// <reference path="globals/require/index.d.ts" />
